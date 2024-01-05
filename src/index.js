a57ca1de78ac7958123dd31d6997c22238717d42
Harshit Satya <harshitsatya5@gmail.com>
2024-01-05 10:24:16 +0530

    changes in comment in index.js

--- src/index.js
+++ src/index.js
@@ -16,7 +16,8 @@ import mongoose from "mongoose";
 import { DB_NAME } from "./constants";
 import express from "express";
 
-const app = express()(async () => {
+const app = express();
+(async () => {
   try {
     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
     app.on("error", (error) => {
